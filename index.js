b3fd07ac7064ea3ac295ebc3f36850dea7a6ac1e
louisececcaldi <comacle.louise@gmail.com>
2020-10-03 22:14:10 +0200

    Revert "creation des fichiers information.js, index.js"
    This reverts commit 63ce16f64ada86c8828233ba071654dc7b2206ec.

--- index.js
+++
@@ -1,9 +0,0 @@
-const myInformation = require('./information.js');
-
-const cowsay = require('cowsay');
-
-console.log(
-    cowsay.think({
-    text: `Hello, I'm ${myInformation.name} from ${myInformation.campus}`,
-})
-);
--- information.js
+++
@@ -1,6 +0,0 @@
-const test ={
-    name: "Louise", 
-    campus: "Lyon",
-};
-
-module.exports = test;
